0bed03f317e9a6e2932750fb0d0849e36e6975b3
elischwat <eli.l.schwat@gmail.com>
2021-03-05 13:09:59 -0800

    delete a bunch of old notebooks

--- create_dems/create_dem_nisqually_1977/run-routine.sh
+++
@@ -1,5 +0,0 @@
-conda activate hsfm
-unset DISPLAY # dem_align.py will try to bring up the final plot for viewing. this is a way to disable that
-nohup python routine.py & # to send the process to the background
-tail -f nohup.out # to watch the log
-htop # to watch the processing
